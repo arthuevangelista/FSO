2e86cc7bf2f0f36927d8e0c20f562a8facbc3845
Arthur Evangelista <arthuevangelista@gmail.com>
2019-04-29 04:32:28 -0300

    Teste realizado com sucesso. Não é possível adicionar as funções que implementam threads em arquivos separados

--- Aulas/Aula 12/exe_semaforo.c
+++ Aulas/Aula 12/exe_semaforo.c
@@ -67,6 +67,25 @@ void return_ticket(int t){
   sem_post(&S);
 } // FIM DO return_ticket
 
+// Subrotina da thread
+void* threadCoop(void* param){
+  int meu_ticket;
+  int fid = (intptr_t) param;
+
+  meu_ticket = get_ticket();
+  if(meu_ticket < 0){
+    fprintf(stderr, "%s\n", "Sem tickets disponíveis no momento!\n");
+    pthread_exit(NULL);
+  }else{
+    fprintf(stderr, "Thread [%d]: Meu ticket é o número %d.\n", fid, meu_ticket);
+    usleep(1000); // 1 segundo
+    return_ticket(meu_ticket);
+    fprintf(stderr, "Thread [%d]: Devolvendo meu ticket número %d!\n", fid, meu_ticket);
+    pthread_exit(NULL);
+  } // FIM DO IF
+} // FIM DA SUBROTINA
+
+
 int main(){
   // Declaração da Thread
   pthread_t minhaThread[NUM_THREAD];
--- Aulas/Aula 12/exemplo.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EXEMPLO_H_
-#define _EXEMPLO_H_
-
-void* threadCoop(void* param);
-
-#endif _EXEMPLO_H_
